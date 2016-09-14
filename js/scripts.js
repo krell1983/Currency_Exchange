13475b6ae6df7ff4f026304d841b844cd8c37340
krell1983 <Piotr.Markiewicz.1983@gmail.com>
2016-09-14 10:43:03 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,15 @@ App.controller('Currency_Exchange_Controller', function ($scope, $http) {
 
         $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22" + $scope.Select_Currency_1 + $scope.Select_Currency_2 + "%22%2C%20%22" + $scope.Select_Currency_2 + $scope.Select_Currency_1 + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
                 .then(function (response) {
-                    //console.log('Status:'+response.status);
                     $scope.CurrencyExcange = response.data.query.results.rate;
-
-                    //console.log('$scope.CurrencyExcange:'+$scope.CurrencyExcange);
 
                     $scope.Currency_Excange_Rate_1 = $scope.CurrencyExcange[0].Rate;
                     $scope.Currency_Excange_Rate_2 = $scope.CurrencyExcange[1].Rate;
-                    //console.log('$scope.Select_Currency_1:'+$scope.Select_Currency_1);
-                    //console.log('$scope.Select_Currency_2:'+$scope.Select_Currency_1);
 
                     $scope.calculate_currency(1);
 
                 }, function (response) {
-                    //console.log('Status:' + response.status);
+                 
                 });
 
 
@@ -29,8 +24,6 @@ App.controller('Currency_Exchange_Controller', function ($scope, $http) {
 
 //** Currency Calculator **
     $scope.calculate_currency = function (select_imput) {
-
-        //console.log('select_imput:'+select_imput);
 
         if (select_imput === 1) {
             $scope.Currency_Imput_2 = Math.round($scope.Currency_Imput_1 * $scope.Currency_Excange_Rate_1 * 100) / 100;
@@ -43,11 +36,10 @@ App.controller('Currency_Exchange_Controller', function ($scope, $http) {
 
     };
 
-//** Load List of currency **
+
     $http.get("currency-list.json")
             .then(function (response) {
                 $scope.currency_list = response.data;
-                //console.log('$scope.currency_list:'+$scope.currency_list.USD.name);
             });
 
     $scope.Load_New_Data();
